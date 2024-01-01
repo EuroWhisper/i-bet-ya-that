48071f9db6581735a720f7840f405ad9feecab59
Laurence Juden <juden@cngroup.dk>
2024-01-01 13:43:00 +0100

    Install and configure Prisma ORM

---
+++ prisma/migrations/20240101123430_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Prediction" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "prediction" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "reminderDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Prediction_pkey" PRIMARY KEY ("id")
+);
